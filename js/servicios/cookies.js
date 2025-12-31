818c48f418139be1358010c619029343e4fec7e9
Btncr13 <betancor13@gmail.com>
2025-12-31 12:34:20 +0100

    Añadido encodeURIComponent para proteger las cookies

--- js/servicios/cookies.js
+++ js/servicios/cookies.js
@@ -5,22 +5,23 @@
 export const GestionCookies = {
 
     // Guarda el array de citas en una cookie llamada 'davante_citas'
-    guardarCitas: function(citas) {
+    guardarCitas: function (citas) {
         // Convertimos el array de objetos a Texto (JSON)
         const datosTexto = JSON.stringify(citas);
-        
+
         // Creamos la cookie para que dure 30 días
         // path=/ asegura que esté disponible en toda la web
-        document.cookie = `davante_citas=${datosTexto}; max-age=${60*60*24*30}; path=/`;
+        // Añadimos encodeURIComponent para proteger caracteres especiales como ; , /
+        document.cookie = `davante_citas=${encodeURIComponent(datosTexto)}; max-age=${60 * 60 * 24 * 30}; path=/`;
     },
 
     // Recupera las citas guardadas
-    obtenerCitas: function() {
+    obtenerCitas: function () {
         const nombre = "davante_citas=";
         const cookiesDecodificadas = decodeURIComponent(document.cookie);
         const arrayCookies = cookiesDecodificadas.split(';');
 
-        for(let i = 0; i < arrayCookies.length; i++) {
+        for (let i = 0; i < arrayCookies.length; i++) {
             let c = arrayCookies[i];
             while (c.charAt(0) === ' ') {
                 c = c.substring(1);
